e5406531028f73027a7b508ba37f89ce11b8346e
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2022-12-19 15:54:14 +0530

    Search Box Designed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ const key = `d4153b342010ef4174468b6454bc0e26`;
 const [Data,setData] = useState([{}])
 
   return (
-    <div>
+    <div className='
+    flex
+    p-[25px]
+     '>
+      <input className='
+      p-4
+      w-[80%]
+      m-auto
+      border-[1px solid]
+      border-gray-200
+      rounded-lg
+      text-base
+      shadow-md
+      shadow-[#888888]
+      focus:outline-none
+      ' placeholder='Enter City Here' />
     </div>
   );
 }
