996def6132496f0efe06474563b5ae27e07f3ceb
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2022-12-22 09:15:56 +0530

    App in CSS

--- src/App.js
+++ src/App.js
@@ -1,55 +1,66 @@
-import { useState } from 'react';
-import Header from './Components/Header'
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-//Variables and useState Hooks
-const key = `d4153b342010ef4174468b6454bc0e26`;
-const [Data,setData] = useState([{}])
-const [City,setCity] = useState("")
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${City}&units=imperial&appid=${key}`
-// getWeather Data
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=d4153b342010ef4174468b6454bc0e26`
 
-const getWeather = (event) =>{
-  if(event.key =='Enter'){
-    fetch(url)
-    .then(response => response.json())
-    .then(data =>{
-      setData(data)
-      setCity("")
-    })
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
   }
-}
-
-
-
 
   return (
-    <div>
-      <Header />
-    <div className='flex flex-col justify-center items-center p-[25px]'>
-      <input className='p-4 w-[80%] m-auto border-[1px solid] border-gray-200 rounded-lg text-base shadow-md shadow-[#888888] focus:outline-none' placeholder='Search City Here' 
-      onChange={e => setCity(e.target.value)}
-      value = {City} onKeyPress ={getWeather}
-      />
-
-      {typeof Data.main ==='undefined'?(
-        <div className='mt-[4rem]'>
-          <p className='text-semibold text-lg	'>Welcome to Weather App Enter a City to Proceed ! 😊 </p>
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter City Here'
+          type="text" />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p className='head'>{data.weather[0].main}</p> : null}
+          </div>
         </div>
-      ):(
-        <div className='mt-[50px] flex flex-col align-center'>
-          <p className='text-[30px] mt-5'>{Data.name}  ,  {Data.sys.country} </p>
-          <div className='flex'>
-          <p className='text-[90px] mt-2'>{Math.ceil(Data.main.temp)} °F</p>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} mph</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <p className='text-[25px] mt-2'><span className='text-orange-500 text-semibold'>Feels Like : </span>{Data.weather[0].main}</p>
-        
-          <p className='text-[25px] mt-2'><span className='text-orange-500 text-semibold'>Wind Speed : </span>{Data.wind.speed} mph</p>
-          </div>
-      )}
+        }
+
+
+
+      </div>
     </div>
-  </div>
   );
 }
 
